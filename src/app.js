23b1d1116cf94ecb67fe53b9fc601bac59281699
Samuel Diaz <90120898+samuelgiorno@users.noreply.github.com>
2025-03-31 19:13:35 +0000

    cambios para entrega lineamientos de codigo

--- src/app.js
+++ src/app.js
@@ -3,24 +3,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// array con excusas
+// Array con excusas
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-function getRandomNumber(a, b) {
-  return Math.floor(Math.random() * (b - a) + a);
+// Generar una excusa aleatoria
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getRandom(anyArray){
-  let max = anyArray.length - 1;
-  let min = 0;
-  let random = getRandomNumber(min, max);
-  return anyArray[random];
+  return anyArray[getRandomNumber(0, anyArray.length - 1)];
 }
+
+function generateExcuse() {
+  let excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}`;
+  return excuse;
+}
+
 
 window.onload = function(){
   console.log("Hello Samuel from the console!");
-  document.querySelector('#excuse').innerHTML = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when) + " " 
-};
+  document.querySelector('#excuse').innerHTML = generateExcuse();
+}
